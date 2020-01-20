8cfe1f4c4651f9bd31eca1979f96184514eeeee1
XyParaCrim <xylome@qq.com>
2020-01-20 19:52:51 +0800

    [newly][feat] * complete file cache

--- src/main/java/collaborative/engine/core/identify/SimpleIdentifier.java
+++ src/main/java/collaborative/engine/core/identify/SimpleIdentifier.java
@@ -2,6 +2,7 @@ package collaborative.engine.core.identify;
 
 import java.io.File;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 /**
  * 实现草稿
@@ -11,24 +12,41 @@ import java.util.UUID;
  */
 public class SimpleIdentifier implements Identifier {
 
+    private final static int MIN_CACHE_CAPACITY_BITS = 5;
+
+    private final static int MAX_CACHE_CAPACITY_BITS = 11;
+
+    private final Cache cache = new Cache(MIN_CACHE_CAPACITY_BITS);
+
     @Override
     public ObjectId newId() {
-        return new ObjectUUID();
+        return new ObjectUUID(cache);
     }
 
     @Override
     public ObjectId toObjectId(String name) {
-        return null;
+        return new ObjectUUID(name, cache);
     }
 
     private static class ObjectUUID implements ObjectId {
 
+        final Cache cache;
         final UUID uuid;
         final File location;
+        final File group;
 
-        public ObjectUUID() {
+        ObjectUUID(Cache cache) {
+            this.cache = cache;
             this.uuid = UUID.randomUUID();
             this.location = fileFor(uuid);
+            this.group = this.location.getParentFile();
+        }
+
+        ObjectUUID(String name, Cache cache) {
+            this.cache = cache;
+            this.uuid = UUID.fromString(name);
+            this.location = fileFor(uuid);
+            this.group = this.location.getParentFile();
         }
 
         @Override
@@ -42,8 +60,23 @@ public class SimpleIdentifier implements Identifier {
         }
 
         @Override
-        public void used() {
+        public void cache() {
+            cache.add(this);
+        }
 
+        @Override
+        public void uncache() {
+            cache.remove(this);
+        }
+
+        @Override
+        public boolean acquire() {
+            return false;
+        }
+
+        @Override
+        public void release() {
+            throw new UnsupportedOperationException();
         }
 
         @Override
@@ -51,11 +84,106 @@ public class SimpleIdentifier implements Identifier {
             return null;
         }
 
-        private static File fileFor(UUID uuid) {
-            String uuidStr = uuid.toString();
-            String group = uuidStr.substring(0, 2);
-            String name = uuidStr.substring(2);
-            return new File(group, name);
+        @Override
+        public String toString() {
+            return uuid.toString();
         }
+    }
+
+    private static final class Cache {
+
+        private final static ObjectUUID REMOVED = new ObjectUUID(null);
+
+        private final static int MAX_TRY_COUNT = 8;
+
+        private final AtomicReferenceArray<ObjectUUID> container;
+
+        private final int bits;
+
+        private final int shift;
+
+        Cache(int bits) {
+            this.bits = bits;
+            this.shift = 32 - bits;
+            this.container = new AtomicReferenceArray<>(1 << bits);
+        }
+
+        void add(ObjectUUID objectId) {
+            int index = index(objectId);
+            for (int n = 0; n < MAX_TRY_COUNT; ) {
+                ObjectUUID uuid = container.get(index);
+                if (objectId.equals(uuid)) {
+                    return;
+                }
+
+                if (container.compareAndSet(index, uuid, objectId)) {
+                    if (uuid == null || uuid.equals(REMOVED)) {
+                        return;
+                    }
+
+                    objectId = uuid;
+                    if (++index == container.length()) {
+                        index = 0;
+                    }
+                    n++;
+                }
+            }
+        }
+
+        boolean contains(ObjectUUID objectId) {
+            int index = index(objectId);
+            for (int n = 0; n < MAX_TRY_COUNT; ) {
+                ObjectUUID uuid = container.get(index);
+                if (uuid == null) {
+                    return false;
+                }
+
+                if (uuid.equals(objectId)) {
+                    return true;
+                }
+
+                if (++index == container.length()) {
+                    index = 0;
+                }
+                n++;
+            }
+            return false;
+        }
+
+        void remove(ObjectUUID objectId) {
+            int index = index(objectId);
+            for (int n = 0; n < MAX_TRY_COUNT; ) {
+                ObjectUUID uuid = container.get(index);
+                if (uuid == null) {
+                    return;
+                }
+
+                if (uuid.equals(objectId)) {
+                    if (container.compareAndSet(index, uuid, REMOVED)) {
+                        return;
+                    } else {
+                        continue;
+                    }
+                }
+
+                if (++index == container.length()) {
+                    index = 0;
+                }
+                n++;
+            }
+        }
+
+        int index(ObjectUUID objectId) {
+            return objectId.uuid.hashCode() >>> shift;
+        }
+    }
+
+    // Util Methods
+
+    private static File fileFor(UUID uuid) {
+        String uuidStr = uuid.toString();
+        String group = uuidStr.substring(0, 2);
+        String name = uuidStr.substring(2);
+        return new File(group, name);
     }
 }
